e825efc0c63b4b9d7f9ba2dc3923ad01c99adbff
Tarek Hallak <HallakTarek7@gmail.com>
2024-07-01 23:49:58 +0300

    Add : the ability to calculate the numbers of nightes.

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,5 +27,12 @@ class Travel extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    public function NumberOfNights() :Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => $attributes['number_of_days'] -1 
+        );
     }
 }
