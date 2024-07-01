9af3fe329f45bea59735238229c7ea43c178fc55
Tarek Hallak <HallakTarek7@gmail.com>
2024-07-02 00:18:08 +0300

    feature: change id into uuid.

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use League\CommonMark\Extension\Attributes\Node\Attributes;
 
 class Tour extends Model
 {
-    use HasFactory;
+    use HasFactory , HasUuids;
 
     protected $fillable = [
         'travel_id',
