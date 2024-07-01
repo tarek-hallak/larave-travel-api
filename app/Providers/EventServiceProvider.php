72bc7926bef3b697861ae6d342cd1d3f4c391f59
Tarek Hallak <HallakTarek7@gmail.com>
2024-07-01 23:17:06 +0300

    Add : The TravelObserver rto the EventServesProvider.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Travel;
+use App\Observers\TravelObserver;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -19,13 +21,16 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
     ];
+    protected $observers = [
+    Travel::class => [TravelObserver::class]
+    ];
 
     /**
      * Register any events for your application.
      */
     public function boot(): void
     {
-        //
+        Travel::observe(TravelObserver::class);
     }
 
     /**
