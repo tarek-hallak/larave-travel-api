608cfe6bde85dd7f77734c69c2e6d83a564e1579
Tarek Hallak <HallakTarek7@gmail.com>
2024-07-02 00:05:49 +0300

    Add : relationship for tours.

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -6,6 +6,7 @@ use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
@@ -20,6 +21,16 @@ class Travel extends Model
         'number_of_days'
     ];
 
+    public function tours(): HasMany
+    {
+        return $this->hasmany(Tour::class);
+    }
+    
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'foreign_key', 'local_key');
+    }
+
     public function sluggable(): array
     {
         return [
@@ -29,10 +40,10 @@ class Travel extends Model
         ];
     }
 
-    public function NumberOfNights() :Attribute
+    public function NumberOfNights(): Attribute
     {
         return Attribute::make(
-            get: fn($value, $attributes) => $attributes['number_of_days'] -1 
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
         );
     }
 }
